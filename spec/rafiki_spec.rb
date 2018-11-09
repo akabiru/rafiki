a2f791547e67dd6e0ac329efe84272f3eda2fd02
Austin Kabiru <makabby@gmail.com>
2018-11-09 10:44:06 +0300

    Genesis

---
+++ spec/rafiki_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rafiki do
+  it "has a version number" do
+    expect(Rafiki::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
